378f1741c4c80ecd74adb5e7ce1d2aa9a1e6e3a6
Greg Kluska <kluskagreg@gmail.com>
2020-06-24 22:32:09 +0100

    Set up auth architecture

---
+++ app/src/main/java/com/codingwithmitch/openapi/ui/auth/AuthViewModel.kt
@@ -0,0 +1,11 @@
+package com.codingwithmitch.openapi.ui.auth
+
+import androidx.lifecycle.ViewModel
+
+class AuthViewModel
+constructor(
+    val authViewModel: AuthViewModel
+): ViewModel()
+{
+
+}

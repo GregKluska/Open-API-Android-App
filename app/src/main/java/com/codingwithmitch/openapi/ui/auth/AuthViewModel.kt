622c4926ddc22f08a2d24b2c81b2188f20054e0d
Greg Kluska <kluskagreg@gmail.com>
2020-06-26 01:08:54 +0100

    Dagger Setup

--- app/src/main/java/com/codingwithmitch/openapi/ui/auth/AuthViewModel.kt
+++ app/src/main/java/com/codingwithmitch/openapi/ui/auth/AuthViewModel.kt
@@ -1,10 +1,13 @@
 package com.codingwithmitch.openapi.ui.auth
 
 import androidx.lifecycle.ViewModel
+import com.codingwithmitch.openapi.repository.auth.AuthRepository
+import javax.inject.Inject
 
 class AuthViewModel
+@Inject
 constructor(
-    val authViewModel: AuthViewModel
+    val authRepository: AuthRepository
 ): ViewModel()
 {
 
